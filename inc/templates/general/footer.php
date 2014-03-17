bee19a0545a8b0632db8e52eb8a4b102269fc9c9
dlhextall <david.lafrance.17@gmail.com>
2014-03-17 19:17:34 -0400

    styling

--- inc/templates/general/footer.php
+++ inc/templates/general/footer.php
@@ -1,7 +1,4 @@
             </div>
-            <!-- <section id="facebook_sharing">
-                  <?php include 'inc/templates/general/_facebook_sharing.php'; ?>
-            </section> -->
         </div>
     </body>
 </html>
