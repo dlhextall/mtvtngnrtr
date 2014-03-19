c1fcbff663de9791f1ad050202ecb00eda0a703c
dlhextall <david.lafrance.17@gmail.com>
2014-03-19 01:13:09 -0400

    fixed the nsfw filtering label

--- inc/templates/general/_bg_chooser.php
+++ inc/templates/general/_bg_chooser.php
@@ -1,4 +1,4 @@
 <li><img src="images/loading.gif" alt="" class="loading"> <a id="new_bg_link" href="#">new</a></li>
 <li><a id="custom_bg_link" href="#">custom</a></li>
-<li><img src="images/loading.gif" alt="" class="loading"> <a id="nsfwfilter_link" href="#"><?php echo ($_COOKIE['filternsfw'] == 0) ? "disable nsfw" : "enable nsfw"; ?></a></li>
+<li><img src="images/loading.gif" alt="" class="loading"> <a id="nsfwfilter_link" href="#"><?php echo ($_COOKIE['filternsfw'] === 0) ? "disable nsfw" : "enable nsfw"; ?></a></li>
 <li><img src="images/loading.gif" alt="" class="loading"> <a id="reset_bg_link" href="#">reset</a></li>
